448ee36013caafdcb2cbb3682f6db8fed318cca5
chengyong <willcy1006@163.com>
2018-12-04 18:39:32 +0800

    Completed Echarts sankey diagram (fetch data from Elasticsearch)

--- public/components/main/main.js
+++ public/components/main/main.js
@@ -4,7 +4,8 @@ import React, { Component } from 'react';
 // 引入 ECharts 主模块
 import echarts from 'echarts/lib/echarts';
 // 引入柱状图
-import  'echarts/lib/chart/bar';
+//import  'echarts/lib/chart/bar';
+import 'echarts';
 // 引入提示框和标题组件
 import 'echarts/lib/component/tooltip';
 import 'echarts/lib/component/title';
@@ -16,7 +17,11 @@ var client = new elasticsearch.Client({
   host: 'localhost:9200'
 });
 
+var client2 = new elasticsearch.Client({
+  host: 'localhost:9200'
+});
 
+console.log(client2);
 console.log(client);
 /*
 client.search({
@@ -76,7 +81,9 @@ export class Main extends React.Component {
       this.setState({ time: resp.data.time });
     });
     
-
+    /*
+      銀行帳戶存款 Bar 圖
+    */
     client.search({
       index: 'bank',
         size: 20,
@@ -125,6 +132,110 @@ export class Main extends React.Component {
       console.trace(error.message);
     });
 
+
+    /*
+      桑基圖
+    */
+   var res = client2.search({
+    index: 'sankeytest6',
+      size: 1000,
+      body: {
+        "aggs" : {
+          "table": {
+            "composite" : {
+              "size": 1000,
+              "sources": [
+                {"stk1": {"terms": {"field": "src.keyword"}}},
+                {"stk2": {"terms": {"field": "mid.keyword"}}},
+                {"stk3": {"terms": {"field": "des.keyword"}}}
+              ]
+            }
+          } 
+        }
+      }
+  }).then(function (body) {
+    var sankey = body.aggregations.table.buckets;
+    console.log(sankey);
+    var sankeyJson = {nodes:[],links:[]};
+
+    var item;
+    
+    var nameArr = [];
+    for(item in sankey){
+      
+      if (nameArr.includes(sankey[item]["key"]["stk1"]) == false){
+        nameArr.push(sankey[item]["key"]["stk1"]);
+      }
+      if (nameArr.includes(sankey[item]["key"]["stk2"]) == false){
+        nameArr.push(sankey[item]["key"]["stk2"]);
+      }
+      if (nameArr.includes(sankey[item]["key"]["stk3"]) == false){
+        nameArr.push(sankey[item]["key"]["stk3"]);
+      }
+      
+    }
+
+    var itemNode;
+    for(itemNode in nameArr){
+      sankeyJson.nodes.push({"name":nameArr[itemNode]})
+    }
+
+    var linkSrcArr = [];
+    var linkMidArr = [];
+    var linkDesArr = [];
+    var linkValueArr = [];
+    for(itemNode in sankey){
+      linkSrcArr.push(sankey[itemNode]["key"]["stk1"]);
+      linkMidArr.push(sankey[itemNode]["key"]["stk2"]);
+      linkDesArr.push(sankey[itemNode]["key"]["stk3"]);
+      linkValueArr.push(sankey[itemNode]["doc_count"])
+    }
+
+    for(itemNode in linkSrcArr){
+      sankeyJson.links.push({"source":linkSrcArr[itemNode],"target":linkMidArr[itemNode],"value":linkValueArr[itemNode]});
+      sankeyJson.links.push({"source":linkMidArr[itemNode],"target":linkDesArr[itemNode],"value":linkValueArr[itemNode]});
+    }
+
+    console.log(sankeyJson);
+    var mySankey = echarts.init(document.getElementById('sankey'));
+
+    mySankey.setOption({
+      title: {
+          text: 'Sankey Diagram'
+      },
+      tooltip: {
+          trigger: 'item',
+          triggerOn: 'mousemove'
+
+      },
+      series: [
+          {
+              type: 'sankey',
+              data: sankeyJson.nodes,
+              links: sankeyJson.links,
+              focusNodeAdjacency: true,
+              itemStyle: {
+                  normal: {
+                      borderWidth: 1,
+                      borderColor: '#aaa'
+                  }
+              },
+              lineStyle: {
+                  normal: {
+                      curveness: 0.5
+                  }
+              }
+          }
+      ]
+    });
+
+    
+    
+ }, function (error) {
+   console.trace(error.message);
+ });
+
+  console.log(res);
     
     /*
     var myChart = echarts.init(document.getElementById('main'));
@@ -163,6 +274,7 @@ export class Main extends React.Component {
             <EuiPageContentBody>
               <EuiText>
               <div id="main" style={{ width: 600, height: 400 }}></div>
+              <div id="sankey" style={{ width: 600, height: 400 }}></div>
               </EuiText>
             </EuiPageContentBody>
           </EuiPageContent>
